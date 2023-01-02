89f9334779203e541ad758e3981b7efbea08c2d9
Amr Ahmed <63152787+amr20012020@users.noreply.github.com>
2023-01-02 03:34:13 +0200

    GPS

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gps/ui/home_screen.dart';
+import 'package:location/location.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+
+}
